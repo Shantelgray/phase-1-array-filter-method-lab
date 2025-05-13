7da4e5820749ce8fde9ff05a93f8e78885da6315
Shantel <Shantelgray24@gmail.com>
2025-05-12 21:12:16 -0400

    adding array filters to this method lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(drivers, names) {
+  return drivers.filter(driver => driver.toLowerCase() === names.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+function matchName(drivers, matchWithPerson) {
+  return drivers.filter(driver => driver.name === matchWithPerson);
+}
